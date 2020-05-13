45d225168d2f4a7076e28e1009497e01d08db62f
andresmauricio <andresacelas@outlook.com>
2020-05-13 11:36:04 -0500

    puerto heroku

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const app = require('./server');
 require('./database');
 
 async function main() {
-    await app.listen(3000);
+    await app.listen( process.env.PORT || 3000);
     console.log('Server running on port 3000');
 }
 
